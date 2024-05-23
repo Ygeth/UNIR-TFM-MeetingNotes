a247f5feed94dafa13d9666d74d2f851fb94ff3a
Ricardo Montaner <ricardo.montaner@sngular.com>
2024-05-23 23:59:23 +0200

    refactor

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
+import os
 import models
-
+from utils.videoUtils import VideoUtils
 model_whisper = models.load_whisper()
 
 def transcript_whisper(audio_path):
     transcript = model_whisper.transcribe(audio_path)
     return transcript
   
-  
-# transcript_whisper("data/Refinamiento_AQ_audio.mp3")
+
+def transcriptVideo(video_path):
+    ## check if video_path.replace(".mp4", "_audio.mp3") exists
+    audio_path = video_path.replace(".mp4", "_audio.mp3")
+    if not os.path.exists(video_path.replace(".mp4", "_audio.mp3")):
+        audio_path = VideoUtils.extract_audio_from_video(video_path, video_path.replace(".mp4", "_audio.mp3"))
+    
+    
+    transcript = transcript_whisper(audio_path)
+    return transcript
+
+
+def main():
+    video_path = "../data/ENDV_intro.mp4"  # replace with your video path
+    transcript = transcriptVideo(video_path)
+    print(transcript)
+
+if __name__ == "__main__":
+    main()
